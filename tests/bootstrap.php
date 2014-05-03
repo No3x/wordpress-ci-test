6bdc3046586af7789f273897a9e9b1373e9ebaa7
No3x <noex_home@yahoo.de>
2014-05-03 12:44:19 +0200

    Test include

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 
+echo "Executing Boostrap Shortcoddes Test Suite" . PHP_EOL;
+
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+
+echo 'Tests folder: ' . $_tests_dir . PHP_EOL . PHP_EOL;
 
 require_once $_tests_dir . '/includes/functions.php';
 
--- tests/test-shortcodes.php
+++ tests/test-shortcodes.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once( './bootstrap-shortcodes.php' );
+//require_once( './bootstrap-shortcodes.php' );
 
 class ShortcodesTest extends WP_UnitTestCase {
 	
