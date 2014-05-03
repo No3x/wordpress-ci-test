fac099baeca5db95da6159338a923fed293e242f
No3x <noex_home@yahoo.de>
2014-05-03 12:39:48 +0200

    Test load method

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,17 +1,12 @@
 <?php
 
-// Activates this plugin in WordPress so it can be tested.
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'wordpress-ci-test/bootstrap-shortcodes.php' ),
-);
-
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../sample-plugin.php';
+	require dirname( __FILE__ ) . '/../bootstrap-shortcodes.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
